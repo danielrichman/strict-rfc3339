1bdde2bc836cd64fea83f67fae970807be68f4fd
Daniel Richman <main@danielrichman.co.uk>
2013-03-25 21:04:55 +0000

    Make setup.py work

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ readme_file = os.path.join(os.path.dirname(__file__), 'README.md')
 readme = open(readme_file).read()
 
 setup(
-    name='strict_rfc3339',
+    name='strict-rfc3339',
     version='0.1',
     author='Daniel Richman, Adam Greig',
     author_email='main@danielrichman.co.uk',
-    packages=['strict_rfc3339'],
+    url='http://www.danielrichman.co.uk/projects/strict-rfc3339.html',
+    py_modules=['strict_rfc3339'],
     description="Strict, simple, lightweight RFC3339 functions",
     long_description=readme,
     license="GNU General Public License Version 3",

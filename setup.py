128242fb1e9fa01e943b1badede0d70f12bc6fe9
Daniel Richman <main@danielrichman.co.uk>
2013-03-25 22:40:07 +0000

    Version 0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ readme = open(readme_file).read()
 
 setup(
     name='strict-rfc3339',
-    version='0.2',
+    version='0.3',
     author='Daniel Richman, Adam Greig',
     author_email='main@danielrichman.co.uk',
     url='http://www.danielrichman.co.uk/projects/strict-rfc3339.html',
